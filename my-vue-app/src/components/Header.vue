dc915c15c18320e19488c5acbfb426c25d98574f
Юльян <ipon7200@gmail.com>
2025-06-04 20:33:38 +0300

    main code

---
+++ my-vue-app/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template> 
+<header> 
+<h1>Мій перший Vue-додаток!</h1> 
+</header> 
+</template>
+<script> 
+export default { 
+name: "HeaderComponent" 
+}; 
+</script>
+<style scoped> 
+header { 
+background: #42b983; 
+color: white; 
+padding: 15px; 
+text-align: center; 
+} 
+</style>
